54a9ab5545cbb12644051800cf1e63154f98aa64
matthew-ritch <mritch3@gatech.edu>
2020-04-01 17:39:56 -0400

    added continuous collection framework and calibration

--- arduino acc/sketch_acc/sketch_acc.ino
+++ arduino acc/sketch_acc/sketch_acc.ino
@@ -3,6 +3,9 @@
  * https://howtomechatronics.com/tutorials/arduino/arduino-and-mpu6050-accelerometer-and-gyroscope-tutorial/
  * 
  * https://learn.sparkfun.com/tutorials/lsm9ds1-breakout-hookup-guide#lsm9ds1-overview
+ * 
+ * accel base code here: https://github.com/sparkfun/SparkFun_LSM9DS1_Arduino_Library/blob/master/src/SparkFunLSM9DS1.cpp
+ * we have 30Kb of memory to work with
  */
  
 #include <SPI.h> // SPI library included for SparkFunLSM9DS1
@@ -27,11 +30,11 @@ imu.settings.device.agAddress = LSM9DS1_AG; // Set ag address to 0x6B
 #define PRINT_CALCULATED
 //#define PRINT_RAW
 #define PRINT_SPEED 250 // 250 ms between prints
+#define CHECK_SPEED 3000 // n ms between checking for frequency/etc.
 static unsigned long lastPrint = 0; // Keep track of print time
 
 // Earth's magnetic field varies by location. Add or subtract
-// a declination to get a more accurate heading. Calculate
-// your's here:
+// a declination to get a more accurate heading. 
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 #define DECLINATION 8.23 // Declination (degrees) in Boulder, CO.
 
@@ -41,7 +44,11 @@ void printAccel();
 void printMag();
 void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
 
+float calibrate [7] = {0, 0, 0, 0, 0, 0} //"hold required null character" in end of variable
+
 void setup()
+
+
 {
   Serial.begin(115200); //start collection at n baud
   Wire.begin(); //join the 12C bus as a master
@@ -56,10 +63,51 @@ void setup()
                    "if the board jumpers are.");
     while (1);
   }
+  //calculate gravity direction/magnitude for first n seconds, to be subtracted later
+  int n = 0
+  while (millis() < 2000) {
+    n+=1;
+    //first 3 hold accelerometer, last 3 hold magnetometer. x y z.
+    calibrate[0]+= imu.calcAccel(imu.ax)
+    calibrate[1]+= imu.calcAccel(imu.ay)
+    calibrate[2]+= imu.calcAccel(imu.az)
+    calibrate[3]+= imu.calcAccel(-imu.gy)
+    calibrate[4]+= imu.calcAccel(-imu.gx)
+    calibrate[5]+= imu.calcAccel(imu.gz)
+  // 
+  }
+  //take average for each one
+  calibrate[0]/= n
+  calibrate[1]/= n
+  calibrate[2]/= n
+  calibrate[3]/= n
+  calibrate[4]/= n
+  calibrate[5]/= n
+  
 }
 
 void loop()
 {
+//decide on some amount of time L to do processing. 5 seconds?
+//decide on some sampling frequency. keep fs*L*3 acceleration points
+//decide if rotate on the fly or rotate every L- would prefer to do on the fly but will change with how fast we can possibly go
+
+//collect new acceleration + mag vectors for L
+
+//rotate each new acceleration vector back to original frame of reference. prefer on the fly
+
+//subtract gravity vector. prefer on the fly
+
+//decide if user was moving for this period
+
+//measure frequency
+
+//measure area swept
+
+
+
+
+  
   // Update the sensor values whenever new data is available
   if ( imu.gyroAvailable() )
   {
@@ -98,6 +146,51 @@ void loop()
 
     lastPrint = millis(); // Update lastPrint time
   }
+
+  if ((lastCheck + CHECK_SPEED) < millis())
+  {
+    printGyro();  // Print "G: gx, gy, gz"
+    printAccel(); // Print "A: ax, ay, az"
+    printMag();   // Print "M: mx, my, mz"
+    #DEFINE accX imu.ax
+    #DEFINE accY imu.ay
+    #DEFINE accZ imu.az
+    // x and y orientation flipped and reversed for magnetometer. see datasheet
+    #DEFINE magX -imu.my
+    #DEFINE magY -imu.mx
+    #DEFINE magZ imu.mz
+    
+    printAttitude(imu.ax, imu.ay, imu.az,
+                  -imu.my, -imu.mx, imu.mz);
+    Serial.println();
+
+    lastCheck = millis(); // Update lastPrint time
+  }
+  
+}
+
+void printGyro()
+{
+  // Now we can use the gx, gy, and gz variables as we please.
+  // Either print them as raw ADC values, or calculated in DPS.
+  Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+  // If you want to print calculated values, you can use the
+  // calcGyro helper function to convert a raw ADC value to
+  // DPS. Give the function the value that you want to convert.
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
 }
 
 void printGyro()
@@ -172,6 +265,9 @@ void printMag()
   Serial.println(imu.mz);
 #endif
 }
+
+
+float 
 
 // Calculate pitch, roll, and heading.
 // Pitch/roll calculations take from this app note:
